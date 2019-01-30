ab11e0f457f3221c0494f8407e28a02a1a187f56
JP Cedeno <juan_cedeno@sprich.com>
2019-01-30 16:44:33 -0500

    Updates

--- cloudscripts/rundocker.sh
+++ cloudscripts/rundocker.sh
@@ -1,1 +1,4 @@
-docker run --rm -it mlenvtest /bin/bash
+#!/usr/bin/bash
+#docker build --tag=mlenvtest .
+# /projects is the same as ~/Dev/projects (host directory must be a location in the HDD)
+docker run --rm -it -v /projects:/var/dataprojectdevops mlenvtest /bin/bash
