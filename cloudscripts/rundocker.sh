d5f9fa19c07237ee17534e0d7a01a64292558706
JP Cedeno <juan_cedeno@sprich.com>
2019-02-21 17:04:57 -0500

    Updated docker file and created make file

--- cloudscripts/rundocker.sh
+++ cloudscripts/rundocker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#docker build --tag=mlenvtest .
+#docker build --squash --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" --build-arg SSH_PUBLIC_KEY="$(cat ~/.ssh/id_rsa.pub)" --tag=mlenvtest .
 
 # Host directory must be a location in the HDD
 docker run --rm -it -v  ~/Dev/projects/data-project-devops:/var/data-project-devops mlenvtest /bin/bash
